e67b6861a034bb94eb84ad5a2cb0a1649947d002
Jamal Bankole <jbankole1212@gmail.com>
2026-07-26 22:11:14 -0400

    Edited environment configurations on client/server

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'https://auth.interviewdocs.net',
-    logoutURL : "https://auth.interviewdocs.net/v2/logout?client_id=J49enmYEIqkzDdzS7gFPXIn4CbPpdKKy&returnTo=https://www.interviewdocs.net"
+    url: 'https://api.interviewdocs.net',
+    logoutURL : "https://api.interviewdocs.net/v2/logout?client_id=J49enmYEIqkzDdzS7gFPXIn4CbPpdKKy&returnTo=https://www.interviewdocs.net"
 };
