207f8e81b5f2d8829bf70f0121336bb9f39add5d
Jamal Bankole <jbankole1212@gmail.com>
2026-08-03 15:18:48 -0400

    Edited prod API url

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://api.interviewdocs.net',
+    url: 'https://api.interviewdocs.net',
     logoutURL : `${import.meta.env.NG_APP_AUTH0_ISSUER}/v2/logout?client_id=${import.meta.env.NG_APP_AUTH0_CLIENT_ID}&returnTo=https://www.interviewdocs.net`
 };
