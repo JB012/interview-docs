f4596bf550578eb66963c17571f2dc6684868879
Jamal Bankole <jbankole1212@gmail.com>
2026-08-04 22:16:05 -0400

    Edited CreateCannedPolicyRequest

--- server/src/main/java/com/interviewdocs/utils/CreateCannedPolicyRequest.java
+++ server/src/main/java/com/interviewdocs/utils/CreateCannedPolicyRequest.java
@@ -1,18 +1,41 @@
 package com.interviewdocs.utils;
 
 import software.amazon.awssdk.services.cloudfront.model.CannedSignerRequest;
+import io.micronaut.context.env.Environment;
+import jakarta.inject.Inject;
 
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 public class CreateCannedPolicyRequest {
+    @Inject
+    Environment environment;
+    
+    private static PrivateKey loadPrivateKeyFromJar() throws Exception {
+        try (InputStream is = CreateCannedPolicyRequest.class.getClassLoader()
+                .getResourceAsStream("private_key_pkcs8.der")) {
+            
+            if (is == null) {
+                throw new IllegalArgumentException("Key file not found in JAR");
+            }
+            
+            byte[] keyBytes = is.readAllBytes();
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePrivate(spec);
+        }
+    }
 
-    public static CannedSignerRequest createRequestForCannedPolicy(String distributionDomainName,
-            String fileName,
-            String privateKeyFullPath, String publicKeyId) throws Exception {
+    public CannedSignerRequest createRequestForCannedPolicy(String distributionDomainName,
+        String fileName, String privateKeyFullPath, String publicKeyId) throws Exception {
+            
         String protocol = "https";
         String resourcePath = "/" + fileName;
 
@@ -20,6 +43,15 @@ public class CreateCannedPolicyRequest {
         Instant expirationDate = Instant.now().plus(1, ChronoUnit.HOURS);
         Path path = Paths.get(privateKeyFullPath);
 
+
+        if (environment.getActiveNames().contains("prod")) {
+            return  CannedSignerRequest.builder()
+                .resourceUrl(cloudFrontUrl)
+                .privateKey(loadPrivateKeyFromJar())
+                .keyPairId(publicKeyId)
+                .expirationDate(expirationDate)
+                .build();
+        }
         return CannedSignerRequest.builder()
                 .resourceUrl(cloudFrontUrl)
                 .privateKey(path)
